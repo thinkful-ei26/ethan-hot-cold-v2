5aa87c71071289b5528b9601606392dfff382800
Ethan Frigon <ethan.frigon@gmail.com>
2018-12-14 13:15:15 -0800

    all redux functionality working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,21 +13,21 @@ export const gameReducer = (state=initialState, action) => {
     const newGuessArray = [...state.guesses, action.guess];
     let newFeedback;
     let difference = Math.abs(state.truth - action.guess);
-        if (state.truth === action.guess){
-            newFeedback = 'winner winner chicken dinner';
-        } else if (difference < 5){
-            newFeedback = 'SCORCHING';
-        } else if (difference < 10){
-            newFeedback = 'bien caliente';
-        } else if (difference < 15){
-            newFeedback = 'hottish';
-        } else if (difference < 25){
-            newFeedback = 'kinda warm i guess?';
-        } else if (difference < 35){
-            newFeedback = 'cold';
-        } else {
-            newFeedback = 'Just embarassing';
-        }
+    if (state.truth === action.guess){
+        newFeedback = 'winner winner chicken dinner';
+    } else if (difference < 5){
+        newFeedback = 'SCORCHING';
+    } else if (difference < 10){
+        newFeedback = 'bien caliente';
+    } else if (difference < 15){
+        newFeedback = 'hottish';
+    } else if (difference < 25){
+        newFeedback = 'kinda warm i guess?';
+    } else if (difference < 35){
+        newFeedback = 'cold';
+    } else {
+        newFeedback = 'Just embarassing';
+    }
     return Object.assign({}, state, {
       guesses: newGuessArray,
       count: newGuessArray.length,
@@ -35,10 +35,7 @@ export const gameReducer = (state=initialState, action) => {
     });
   }
   else if(action.type === actions.NEW_GAME){
-    return Object.assign({}, initialState,  
-      {
-        truth: Math.ceil(Math.random() * 100)    
-      }
+    return Object.assign({}, initialState, {truth: Math.ceil(Math.random() * 100)}
     );
   }
   else if(action.type === actions.OPEN_WHAT || action.type === actions.CLOSE_WHAT){
